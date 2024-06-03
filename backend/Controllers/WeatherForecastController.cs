00de18d103616f57955586c44484d4d409ea07ab
mon00kus <raulmoncadac@gmail.com>
2024-06-03 23:04:18 +0200

    Second commit

--- backend/Controllers/WeatherForecastController.cs
+++ backend/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace backend.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Other"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
