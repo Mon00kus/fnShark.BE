f3eb4cdea87ed5413e62166d1c134f6d7916495c
mon00kus <raulmoncadac@gmail.com>
2024-06-06 23:28:08 +0200

    Stock controller ver 1.0

---
+++ backend/Controllers/StockController.cs
@@ -0,0 +1,36 @@
+﻿using backend.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("/api/[Controller]")]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public StockController( ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stock = _context.Stocks.ToList();
+            return Ok(stock);
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var stock = _context.Stocks.Find(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
+    }
+}
